d7c70b20935691d132f9a4842552f4c41e52d301
jangchaehoon <61855855+jangchaehoon@users.noreply.github.com>
2020-07-23 12:21:35 +0900

    Update BoardListImpl.java

--- src/main/java/model/BoardListImpl.java
+++ src/main/java/model/BoardListImpl.java
@@ -1,16 +1,33 @@
 package model;
 
 import java.util.List;
-
+import java.util.Map;
 import org.springframework.stereotype.Service;
+
 @Service
 public interface BoardListImpl {
-	int selectOrder(String location);
-
-	void createboard(BoardListDTO boardListDTO);
-
-	List<BoardListDTO> selectBoard(String location);
 	
+	int selectMaxOrder(String location);
+	
+	String selectLocation(String idx);
+	
+	int selectorder(String idx);
+	
+	List<Map<String, Object>> selectLocationOrder();
+	
+	void createboard(BoardListVO boardListVO);
+
+	List<BoardListVO> selectBoard(String location);
+	
+	void editboard(BoardListVO boardListVO);
+	
+	void deleteboard(String idx);
+	
+	void updateOrder(String location, int boardorder);
+	
+	void updateOrder1(String idx);
+	void updateOrder2(ParameterVO parameterVO);
+	void updateOrder3(BoardListVO boardListVO);
 	//bname 검색
-	List<BoardListDTO> selectBname();
+	List<BoardListVO> selectBname();
 }

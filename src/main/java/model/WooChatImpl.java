ef9920e641f8bb4988054f64a8c8e3c03b370f07
jangc <jangchaehoon@naver.com>
2020-07-26 19:26:51 +0900

    20/07/26

--- src/main/java/model/WooChatImpl.java
+++ src/main/java/model/WooChatImpl.java
@@ -1,8 +1,13 @@
 package model;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface WooChatImpl {
 	public List<WooChatRoomVO> selectRoom(String sellerid);
 	public List<WooChattingVO> selectLastChat(String roomidx);
+	public void saveMessage(WooChattingVO chattingVO);
+	public void updateLastChatTime(String chatroomidx, Timestamp lastChat);
+	public List<WooChattingVO> selectChatting(String chatroomidx);
 }
